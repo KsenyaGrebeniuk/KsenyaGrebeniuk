90d6b1e2c510f1776c0d08de3472cb83b8a058fa
KsenyaGrebeniuk <116118029+KsenyaGrebeniuk@users.noreply.github.com>
2022-12-21 15:18:48 +0200

    Add files via upload

---
+++ numberM.js
@@ -0,0 +1,47 @@
+/*const numbers = [1,2,3,4,5,6,7,8,9];
+let even = 0 // четное
+let odd = 0 // не четное
+for ( let i = 0; i <= numbers.length; i ++){
+    if (i % 2 == 0) {
+       even = i
+       even = i + even
+    }
+    if (i % 2 !== 0) {
+        odd = i
+        odd = odd + i
+    } 
+    console.log(length.odd)
+} */
+
+/*const numbers = [1,2,3,4,5,6,7,8,9];
+let odd = 0 // не четное
+for ( let i = 0; i <= numbers.length; i ++) {
+    if (i % 2 !== 0) {
+    
+    return odd.length
+}
+} */
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+let result(toString) = 0
+for ( let i = 1; i <= numbers.length; i ++){
+        if (i % 2 !== 0){
+            result = i
+        }
+        console.log (result)
+        
+    }
+     
+  
+    
+
+   /* const numbers = [1,2,3,4,5,6,7,8,9];
+    let even = 0 // четное
+    for ( let i = 0; i <= numbers.length; i ++){
+        if (i % 2 == 0) {
+          
+           even = i + even
+        }
+    } 
+    console.log(even)*/
+
